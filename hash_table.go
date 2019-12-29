dfbe830cefff2c73c74224845d6d0f825d7ee632
zoomie <andrew.arderne@navenio.com>
2019-12-29 17:23:18 +0000

    made custom hash table, still need to implement the string to int sha

--- hash_table.go
+++ hash_table.go
@@ -1,21 +1,50 @@
 package main
 
-type hashTable map[string]int64
+const initArrLength = 5
 
-func (h hashTable) add(key string, value int64) {
-	if h == nil {
-		h = make(map[string]int64)
-	}
-	h[key] = value
+type htuple struct {
+	key   string
+	value int64
 }
 
-func (h hashTable) get(key string) (int64, bool) {
-	if h == nil {
-		return 0, false
-	}
-	value, ok := h[key]
-	if !ok {
-		return 0, false
-	}
-	return value, true
+type valuesArray [][]htuple
+
+type hashTable struct {
+	length int
+	array  valuesArray
 }
+
+func hashString(s string, lenght int) int {
+	// need to get sha implementation
+	return 0
+}
+
+func (h *hashTable) add(key string, value int64) {
+	index := hashString(key, h.length)
+	if h.array == nil {
+		h.array = make(valuesArray, initArrLength)
+	}
+	for i := range h.array[index] {
+		if h.array[index][i].key == key {
+			h.array[index][i].value = value
+			return
+		}
+	}
+	newTuple := htuple{key: key, value: value}
+	h.array[index] = append(h.array[index], newTuple)
+}
+
+func (h *hashTable) get(key string) (int64, bool) {
+	index := hashString(key, h.length)
+	for _, tup := range h.array[index] {
+		if tup.key == key {
+			return tup.value, true
+		}
+	}
+	return 0, false
+}
+
+// func main() {
+// 	h := hashTable{}
+// 	h.add("key", 543)
+// }
